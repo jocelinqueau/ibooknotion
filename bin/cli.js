d15d5364af5c39762d6634f10c238e37717df26e
jocelinqueau <queau.j@gmail.com>
2024-06-11 21:38:29 +0200

    fixing esm errors

---
+++ bin/cli.js
@@ -0,0 +1,75 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const minimist_1 = __importDefault(require("minimist"));
+const prompts_1 = __importDefault(require("prompts"));
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const args = (0, minimist_1.default)(process.argv.slice(2));
+        const message = `
+  Invalid option
+  Usage: ibooknotion [option]?
+
+  ibooknotion will prompt a menu
+
+  Options:
+    --export            Export iBooks notes to JSON, creating a file in the current directory
+    --pageId=<id>       Create a Notion database with the iBooks notes
+    --databaseId=<id>   Update the current Notion database with the fresh data
+  `;
+        if (Object.keys(args).length > 3) {
+            console.log(message);
+            process.exit(0);
+        }
+        if (Object.keys(args).length === 2) {
+            if (args.export) {
+                console.log('exporting');
+                process.exit(0);
+            }
+            else if (args.pageId) {
+                console.log('creating');
+                process.exit(0);
+            }
+            else if (args.databaseId) {
+                console.log('updating');
+                process.exit(0);
+            }
+            else {
+                console.log(message);
+                process.exit(0);
+            }
+        }
+        const { option } = yield (0, prompts_1.default)({
+            type: 'select',
+            name: 'option',
+            message: 'What do you want to do?',
+            initial: 0,
+            choices: [
+                { title: "Export my book annotation to json 💽", value: "export" },
+                { title: "Create a notion database with my ibooks notes 🏗️", value: "create" },
+                { title: "Update my current ibooks notion database with the fresh data 🌱", value: "update" }
+            ],
+        });
+        if (option === 'export') {
+            console.log('exporting');
+        }
+        if (option === 'create') {
+            console.log('creating');
+        }
+        if (option === 'update') {
+            console.log('updating');
+        }
+    });
+}
+main().catch(console.error);
